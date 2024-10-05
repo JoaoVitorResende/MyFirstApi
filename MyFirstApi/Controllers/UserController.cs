680b271e0c4d172af20b84a9e15c7cd9b531ea3a
joao vitor resende <54758120+JoaoVitorResende@users.noreply.github.com>
2024-10-05 09:38:09 -0300

    First commit

---
+++ MyFirstApi/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Comunication.Requests;
+using MyFirstApi.Comunication.Responses;
+namespace MyFirstApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [Route("{Id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetByIdAndNickname([FromRoute] int Id)
+    {
+        var user = new User 
+        {
+            Id = 1,
+            Age = 20,
+            Name = "Test"
+        };
+        return Ok(user);
+    }
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+    public IActionResult CreateUser([FromBody] RequestRegisterUserJson request)
+    {
+        var response = new ResponseRegisteredUserJson
+        {
+            Id = 1,
+            Name = request.Name
+        };
+        return Created(string.Empty, response);
+    }
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromBody] RequestUpdateUserProfileJson request)
+    {
+        return NoContent();
+    }
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Delete()
+    {
+        return NoContent();
+    }
+    [HttpGet]
+    [ProducesResponseType(typeof(List<User>), StatusCodes.Status200OK)]
+    public IActionResult GetAll()
+    {
+        var response = new List<User>()
+        {
+            new User{Id = 2, Age = 2 , Name = "Test"},
+            new User{Id = 3, Age = 3 , Name = "Test2"}
+        };
+        return Ok(response);
+    }
+    [HttpPut("ChangePassword")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult ChangePassword([FromBody] RequestChangePasswordJson request)
+    {
+        return NoContent();
+    }
+}
